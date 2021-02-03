9814a1d6be94598abfbfacafc5b23bd2d65a6bcc
Jay Rajput <74800982+jay-rajput30@users.noreply.github.com>
2021-02-03 13:18:40 +0530

    style: added semantic html and CSS for entire project

--- src/App.js
+++ src/App.js
@@ -3,8 +3,42 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <div className="container">
+        <h1>cash register</h1>
+        <input type="number" className="bill-txtbox" />
+        <div className="radio-group">
+          <div className="radio-group_item">
+            <input type="radio" id="one" value="1" name="cash" />
+            <label for="one">1</label>
+          </div>
+          <div className="radio-group_item">
+            <input type="radio" id="five" value="5" name="cash" />
+            <label for="one">5</label>
+          </div>
+          <div className="radio-group_item">
+            <input type="radio" id="ten" value="10" name="cash" />
+            <label for="one">10</label>
+          </div>
+          <div className="radio-group_item">
+            <input type="radio" id="twenty" value="20" name="cash" />
+            <label for="one">20</label>
+          </div>
+          <div className="radio-group_item">
+            <input type="radio" id="one-hundred" value="100" name="cash" />
+            <label for="one">100</label>
+          </div>
+          <div className="radio-group_item">
+            <input type="radio" id="five-hundred" value="500" name="cash" />
+            <label for="one">500</label>
+          </div>
+          <div className="radio-group_item">
+            <input type="radio" id="two-thousand" value="2000" name="cash" />
+            <label for="one">2000</label>
+          </div>
+        </div>
+        <button className="calculate-btn">calculate</button>
+        <article></article>
+      </div>
     </div>
   );
 }
